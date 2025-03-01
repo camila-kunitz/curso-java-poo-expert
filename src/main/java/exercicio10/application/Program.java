48373939ac7d1d293f5605ef4ff399c9f20dc017
Camila Kunitz <camila.kunitz@gmail.com>
2025-03-01 19:23:09 -0300

    Exercício 01 Herança e Polimorfismo

---
+++ src/main/java/exercicio10/application/Program.java
@@ -0,0 +1,56 @@
+package exercicio10.application;
+
+import exercicio10.entities.ImportedProduct;
+import exercicio10.entities.UsedProduct;
+import exercicio10.entities.Product;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Product> products = new ArrayList<>();
+
+        System.out.print("Enter the number of products: ");
+        int numProducts = sc.nextInt();
+
+        for (int i = 0; i < numProducts; i++) {
+            System.out.println("Product #" + (i + 1) + " data: ");
+            System.out.print("Common, used or imported (c/u/i)? ");
+            char response = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+
+            if (response == 'i') {
+                System.out.print("Customs fee: ");
+                double customsFee = sc.nextDouble();
+                products.add(new ImportedProduct(name, price, customsFee));
+            } else if (response == 'c') {
+                products.add(new Product(name, price));
+            } else {
+                System.out.print("Manufacture date (DD/MM/YYYY): ");
+                LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                products.add(new UsedProduct(name, price, date));
+            }
+        }
+
+        System.out.println();
+        System.out.println("ETIQUETAS DE PREÇO:");
+        for (Product prod : products) {
+            System.out.println(prod.priceTag());
+        }
+
+        sc.close();
+
+    }
+}
